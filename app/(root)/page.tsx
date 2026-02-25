a8fce2c541e3ee913cee1c7ce01361f947c5feea
BuiltByKeith <builtbykeith.dev@gmail.com>
2026-02-25 11:59:02 +0800

    first and initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main>
+      <div className="text-5xl">Welcome to my web portfolio</div>;
+    </main>
+  );
+};
+
+export default Home;
---
+++ app/loader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loader = () => {
+  return <div>____ spinner...</div>;
+};
+
+export default Loader;
